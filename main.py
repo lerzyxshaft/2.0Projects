8ff27e1572af76acc08e48c665748ce67e44892a
Maksym Kozlovskyi <zxcshaft@gmail.com>
2025-06-01 22:44:14 +0200

    ToDo v2

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
+import time
+
 tasks=[]
 
 def add_function():
     function_to_add = input("Fill this land with task you want to add:")
     tasks.append(function_to_add)
     print(tasks)
+    time.sleep(1)
+
 
 def delete_tasks():
-    delete_task = input("Choose which funktion you want to delete:")
+    print(tasks)
+    delete_task = int(input("Choose which ToDo you want to delete:"))
     tasks.remove(delete_task)
     print(tasks)
 
 def list_tasks():
-    print(tasks)
+    if not tasks:
+        print("Your ToDo list is empty!")
+        time.sleep(1)
+    else:
+        print(tasks)
+        time.sleep(1)
 
 def exit_command():
     quit()
@@ -27,14 +37,21 @@ if __name__=="__main__":
         print("3. List tasks")
         print("4. Quit")
 
-        choice = input("Enter your choice")
+        choice = input("Enter your choice: ")
 
         if choice == "1":
             add_function()
         elif choice == "2":
-            delete_tasks()
+            if not tasks:
+                print("Your ToDo list is empty")
+                time.sleep(1)
+            else:
+                delete_tasks()
+                time.sleep(1)
+
         elif choice == "3":
             list_tasks()
+            time.sleep(1)
         elif choice == "4":
             exit_command()
         else:
